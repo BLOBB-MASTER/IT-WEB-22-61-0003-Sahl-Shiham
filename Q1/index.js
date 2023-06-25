e28a090be6709c5adc3574c45b941ce06a4a47ae
BLOBB-MASTER <132095860+BLOBB-MASTER@users.noreply.github.com>
2023-06-25 15:01:10 +0530

    Add files via upload

---
+++ Q1/index.js
@@ -0,0 +1,62 @@
+window.addEventListener('scroll',reveal);
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i=0; i<reveals.length; i++){
+        var windowheight= window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        // else{
+        //     reveals[i].classList.remove('active');
+        // }       
+    }
+}
+
+const form = document.getElementsByClassName('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+
+
+
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const emailValue = email.value.trim();
+
+if(emailValue === '') {
+    setError(email, 'Email is required');
+} else if (!isValidEmail(emailValue)) {
+    setError(email, 'Provide a valid email address');
+} else {
+    setSuccess(email);
+}
